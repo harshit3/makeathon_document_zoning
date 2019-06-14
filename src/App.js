320c65a2d68d8fd9c478aeff98f6280b0631b8c0
Harshit <agrawalharshit16@gmail.com>
2019-06-14 22:44:38 +0530

    adding loading spinner and file uploader

--- src/App.js
+++ src/App.js
@@ -2,44 +2,80 @@ import React, { useState } from 'react';
 import './App.css';
 import pdf from './demo.pdf';
 import FileUploader from './FileUploader/FileUploader';
+import LoadingSpinner from './LoadingSpinner';
 
 function App() {
-  const firstname = useFormInput('');
-  const lastname = useFormInput('');
-  const contactNumber = useFormInput('');
-  const emailAddress = useFormInput('');
 
-  return (
-    <div className="App">
-      <div className='online-form-container'>
-        <FileUploader></FileUploader>
-        <form>
-          <Form name={firstname} label='First Name' />
-          <Form name={lastname} label='Last Name' />
-          <Form name={contactNumber} label='Contact Number' />
-          <Form name={emailAddress} label='Email Address' />
-        </form>
+  const [ fileStatus, setFileStatus ] = useState(1);
+  const [ fileData, setFileData ] = useState({
+    fileBase64: '',
+    type:''
+  })
+
+  const proposed_name = useFormInput('');
+  const proposed_nationality = useFormInput('');
+  const proposed_age = useFormInput('');
+  const proposed_emailAddress = useFormInput('');
+  const proposer_name = useFormInput('');
+  const proposer_age = useFormInput('');
+
+  function handleFileChange(fileStatus, fileData) {
+    setFileStatus(fileStatus);
+    setFileData(fileData);
+  }
+
+  if(fileStatus===1) {
+    return(
+      <FileUploader handleChange={handleFileChange}/>
+    );  
+  } else if(fileStatus===2) {
+    return(
+      <LoadingSpinner />
+    )
+  } else{
+    return (
+      <div className="App">
+        <div className='online-form-container'>
+          <hr/>
+          <h2 className='header'>Online Form</h2>
+          <hr/>
+          <form>
+            <div className='section-name'>Proposed/Insured</div>
+            <Form valueObject={proposed_name} label='Name' id='proposed-name' />
+            <Form valueObject={proposed_age} label='Age' id='proposed-age' />
+            <Form valueObject={proposed_emailAddress} label='Email Address' id='proposed-email' />
+            <Form valueObject={proposed_nationality} label='Nationality' id='proposed-nationality' />
+  
+            <div className='section-name'>Proposer</div>
+            <Form valueObject={proposer_name} label='Name' id='proposer-name' />
+            <Form valueObject={proposer_age} label='Age' id='proposer-age' />
+          </form>
+        </div>
+        <div className='scanned-form-container'>
+          <embed src={pdf} height='100%' width='100%' />
+        </div>
       </div>
-      <div className='scanned-form-container'>
-        <embed src={pdf} height='100%' width='100%' />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 function useFormInput(initialValue) {
   const [value, setValue] = useState(initialValue);
   return {
     value,
-    onChange: (e) => setValue(e.target.value)
+    onChange: (e) => {
+      setValue(e.target.value)
+    }
   }
 }
 
-function Form({ name, label }) {
-  return (
-    <div>
-      <label>{label}: </label>
-      <input type='text' name='name' {...name} />
+function Form({valueObject, label}) {
+  return(
+    <div className='input-conatiner'>
+      <div></div>
+      <label className='label'>{label} </label>
+      <input className='input' type='text' {...valueObject} />
+      <div></div>
     </div>
   );
 }
